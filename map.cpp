14fa8dc2517b7533efcadee661afb875c50a7619
asavage1 <asavag04@tufts.edu>
2016-10-14 23:50:22 -0400

    Map files

---
+++ map.cpp
@@ -0,0 +1,91 @@
+/*
+ * map.h
+ * Polyhack 2016, 10/14/16
+ *
+ * Generates a two dimensional array of characters based on user input (or if
+ * none is provided, uses default sizes) that represents a race course for
+ * the rASCIIng program.
+ */
+
+#include "math.h"
+#include "time.h"
+#include "map.h"
+
+Map::Map()
+{
+        rows = DEFAULT_ROWS;
+        cols = DEFAULT_COLS;
+        newMap();
+}
+
+Map::Map(int width, int height)
+{
+        /* Make sure parameters are reasonable */
+        rows = width;
+        cols = height;
+        newMap();
+}
+
+Map::~Map()
+{
+        for (int i = 0; i < rows; i++) {
+                delete [] map[i];
+        }
+        delete [] map;
+}
+
+void Map::newMap()
+{
+        map = new char*[rows];
+        for (int i = 0; i < rows; i++) {
+                map[i] = createMapRow(i - 1);
+        }
+}
+
+char *Map::getMapLine(int i)
+{
+        return map[i];
+}
+
+int Map::getHeight()
+{
+        return rows;
+}
+
+int Map::getWidth()
+{
+        return cols;
+}
+
+char *Map::createMapRow(int i)
+{
+        if (i < 0) {
+                return createFirst();
+        }
+        return createNext(i);
+}
+
+char *Map::createFirst()
+{
+        char *row = new char[cols];
+        for (int i = 0; i < cols; i++) {
+                if (i < (cols - ROAD_WIDTH) / 2 - 1) {
+                        row[i] = OOB;
+                } else if (i == (cols - ROAD_WIDTH) / 2 - 1) {
+                        row[i] = B_STR;
+                } else if (i < (cols + ROAD_WIDTH) / 2) {
+                        row[i] = RD;
+                } else if (i == (cols + ROAD_WIDTH) / 2) {
+                        row[i] = B_STR;
+                } else {
+                        row[i] = OOB;
+                }
+        }
+        return row;
+}
+
+char *Map::createNext(int lastRow)
+{
+        srand(time(NULL));
+        
+}
