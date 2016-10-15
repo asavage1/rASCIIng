4d683996c1d58ca0b938425e7f473dd9b4023139
asavage1 <asavag04@tufts.edu>
2016-10-15 02:08:54 -0400

    Randomness

---
+++ test_map.cpp
@@ -0,0 +1,22 @@
+/*
+ * test_map.cpp
+ * Polyhack 2016, 10/14/16
+ *
+ * Tests created array and get functions for map function
+ */
+
+#include <iostream>
+#include <string>
+#include "map.h"
+
+using namespace std;
+
+int main()
+{
+        Map map(10, 80);
+        for (int i = 0; i < 10; i++) {
+                string str = map.getMapLine(i);
+                cout << str << endl;
+        }
+        return 0;
+}
