0ee6962c9cb1e338b2daea4c8b329ec4a002e6a7
Andrew Savage <Andrew@wr-130-64-98-183.medford.tufts.edu>
2016-10-15 11:36:47 -0400

    ncurses2

--- Makefile
+++ Makefile
@@ -6,8 +6,8 @@ CXX      = g++
 racing: game.cpp map.cpp parse.cpp print_racing.cpp rASCIIng.cpp game.h map.h parse.h print_racing.h
 	${CXX} -o racing game.cpp map.cpp parse.cpp print_racing.cpp rASCIIng.cpp
 
-racing_mac: game.cpp map.cpp parse_ncurses.cpp print_racing.cpp rASCIIng.cpp game.h map.h parse.h print_racing.h
-	${CXX} -o racing_mac game.cpp map.cpp parse_ncurses.cpp print_racing.cpp rASCIIng.cpp
+racing_mac: game_mac.cpp map.cpp parse_ncurses.cpp print_racing.cpp rASCIIng.cpp game.h map.h parse.h print_racing.h
+	${CXX} -lncurses -o racing_mac game_mac.cpp map.cpp parse_ncurses.cpp print_racing.cpp rASCIIng.cpp
 
 test_ncurses: test_ncurses.cpp
 	${CXX} -lncurses -o test_ncurses test_ncurses.cpp
---
+++ game_mac.cpp
@@ -0,0 +1,44 @@
+// Nick, Iris, Leah, Andrew, and Elias
+// 10/14/16
+//
+// code for race game class
+
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include "game.h"
+#include "parse.h"
+#include "print_racing.h"
+
+using namespace std;
+
+Game::Game()
+{
+	player.finished = false;
+	player.row = 4;
+	player.col = map.getWidth() / 2;
+	player.penalty = 0;
+	seconds = 0;
+}
+
+void Game::play()
+{
+	bool i = false;
+	time_t startTime = time(NULL);
+	print_race(&map, player);
+	while (!player.finished) {
+		player = updatePosition(player, map);
+		if (i++ % 2) {
+			print_race(&map, player);
+		}
+		usleep(PAUSE * 100);
+	}
+	time_t endTime = time(NULL);
+	seconds = difftime(endTime, startTime) + player.penalty;
+}
+
+void Game::printResults()
+{
+	std::cout << "\nYour time was " << seconds << " seconds! Good job!";
+}
